441e416487a7ed3e776510cb2e1d6908ddc22229
Hoang-Anh-Tai-11225655 <128773369+Hoang-Anh-Tai-11225655@users.noreply.github.com>
2024-06-17 21:48:06 +0700

    Initial test

---
+++ config.js
@@ -0,0 +1,8 @@
+const nameGirl = 'Huy';
+const giftUrl = '';
+const eventName = 'Chúc Mừng sinh nhật';
+const titleCard = 'Tặng Huy';
+const contentCard = 'Một tuổi mới đã đến với biết bao điều mới lạ đang chờ đón Huy ở phía trước, hãy tiến lên và giành lấy những thứ thuộc về mình. Là một trưởng ban mẫu mực với một tấm lòng bao dung mẫu mực, cao cả, Huy đã dành cho những thành viên trong ban của mình những điều tuyệt vời nhất. Thật may mắn vì đã được làm việc với Huy trong quãng thời gian vừa qua. Chúc Huy sang tuổi mới sẽ tuyển được thật nhiều nhân sự, không bị ứng viên bùng, mạnh khoẻ, thành công hạnh phúc và đạt được nhiều thành công';
+const giftImage = 'Screen Shot 2024-06-17 at 21.20.16.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
